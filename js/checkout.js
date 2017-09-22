10cfccb26df652ef456d0a2d8ac741f44fc6b1a4
Kevin Springer <KevinUSCU@outlook.com>
2017-09-22 11:29:12 -0700

    Make checkbox for copying shipping info to billing functional.

--- js/checkout.js
+++ js/checkout.js
@@ -90,6 +90,46 @@ var cleave = new Cleave('#cvc', {
     numericOnly: true
 });
 
+//Copy Shipping to Billing
+let copyCheckbox = document.getElementById("copy")
+copyCheckbox.addEventListener("change", function(event) {
+    let firstName1 = document.querySelector(".shipping-billing #firstName1")
+    let lastName1 = document.querySelector(".shipping-billing #lastName1")
+    let company1 = document.querySelector(".shipping-billing #company1")
+    let address1 = document.querySelector(".shipping-billing #address1")
+    let address2 = document.querySelector(".shipping-billing #address2")
+    let city1 = document.querySelector(".shipping-billing #city1")
+    let state1 = document.querySelector(".shipping-billing #state1")
+    let zip1 = document.querySelector(".shipping-billing #zip1")
+    let firstName2 = document.querySelector(".shipping-billing #firstName2")
+    let lastName2 = document.querySelector(".shipping-billing #lastName2")
+    let company2 = document.querySelector(".shipping-billing #company2")
+    let address3 = document.querySelector(".shipping-billing #address3")
+    let address4 = document.querySelector(".shipping-billing #address4")
+    let city2 = document.querySelector(".shipping-billing #city2")
+    let state2 = document.querySelector(".shipping-billing #state2")
+    let zip2 = document.querySelector(".shipping-billing #zip2")
+    if (event.target.checked) { //copy shipping to billing
+        firstName2.value = firstName1.value
+        lastName2.value = lastName1.value
+        company2.value = company1.value
+        address3.value = address1.value
+        address4.value = address2.value
+        city2.value = city1.value
+        state2.value = state1.value
+        zip2.value = zip1.value
+    } else if (!event.target.checked) { //reset billing
+        firstName2.value = ""
+        lastName2.value = ""
+        company2.value = ""
+        address3.value = ""
+        address4.value = ""
+        city2.value = ""
+        state2.value = ""
+        zip2.value = ""        
+    }
+})
+
 //Form Submission and Validation
 // let buyButton = document.getElementById("buy")
 // buyButton.addEventListener("click", function() {
